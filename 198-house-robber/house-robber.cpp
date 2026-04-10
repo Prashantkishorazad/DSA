63b5dfda91a1f76b159bfaa8b51bbf3cbea91b16
Prashant Kishor <99216303+Prashantkishorazad@users.noreply.github.com>
2026-04-10 14:09:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (76.29%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1 = 0; // dp[i-1]
+        int prev2 = 0; // dp[i-2]
+
+        for (int num : nums) {
+            int temp = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+
+        return prev1;
+    }
+};
