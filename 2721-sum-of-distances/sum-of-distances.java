743850192693901f9b3e7834eb2307440f2efe67
Prashant Kishor <99216303+Prashantkishorazad@users.noreply.github.com>
2026-04-23 09:39:51 +0530

    Time: 21 ms (83.86%) | Memory: 122.3 MB (45.74%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        long[] res = new long[n];
+        
+        // Step 1: group indices
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        
+        // Step 2: process each group
+        for (List<Integer> list : map.values()) {
+            int size = list.size();
+            
+            long[] prefix = new long[size + 1];
+            for (int i = 0; i < size; i++) {
+                prefix[i + 1] = prefix[i] + list.get(i);
+            }
+            
+            for (int i = 0; i < size; i++) {
+                int idx = list.get(i);
+                
+                long left = (long) idx * i - prefix[i];
+                long right = (prefix[size] - prefix[i + 1]) - (long) idx * (size - i - 1);
+                
+                res[idx] = left + right;
+            }
+        }
+        
+        return res;
+    }
+}
