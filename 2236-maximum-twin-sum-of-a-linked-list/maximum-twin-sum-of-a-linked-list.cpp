b9180f6a7a7dd5cfaea1edf8103d9a4fbb5829fd
Prashant Kishor <99216303+Prashantkishorazad@users.noreply.github.com>
2026-06-14 14:08:44 +0530

    Time: 4 ms (56.70%) | Memory: 124.5 MB (49.62%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        // Step 1: Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode* prev = NULL;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        // Step 3: Calculate max twin sum
+        int maxSum = 0;
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            maxSum = max(maxSum, first->val + second->val);
+            first = first->next;
+            second = second->next;
+        }
+
+        return maxSum;
+    }
+};
